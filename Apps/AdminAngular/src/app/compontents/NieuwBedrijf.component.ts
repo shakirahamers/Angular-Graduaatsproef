657c9e11a8b6eedba2d55c3aac55605634c24eb9
kira <104847182+shakirahamers@users.noreply.github.com>
2023-06-09 22:39:36 +0200

    nieuw parkeercontract

--- Apps/AdminAngular/src/app/compontents/NieuwBedrijf.component.ts
+++ Apps/AdminAngular/src/app/compontents/NieuwBedrijf.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BedrijfService } from '../services/bedrijf.service';
 
@@ -7,43 +9,93 @@ import { BedrijfService } from '../services/bedrijf.service';
   template: `
   <h1 class="text-center mb-6">Bedrijf aanmaken</h1>
     <div class="flex flex-col justify-center items-center h-full">
+    <form [formGroup]="bedrijfForm" (ngSubmit)="createBedrijf()" class="w-full max-w-md">
         <div>
-            <label for="bedrijfsnaam" class="font-semibold mt-5 block mb-2 text-sm text-gray-500">Bedrijfsnaam</label>
-            <input type="text" id="bedrijfsnaam" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" [(ngModel)]="bedrijf.naam" required>
-
-            <label for="btwnummer" class="block mb-2 text-sm font-semibold text-gray-500">BTW-nummer</label>
-            <input type="text" id="btwnummer" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" [(ngModel)]="bedrijf.btwNummer" required>
-
-            <label for="adres" class="block mb-2 text-sm font-semibold text-gray-500">Adres</label>
-            <input type="text" id="adres" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" [(ngModel)]="bedrijf.adres"  required>
-
-            <label for="telnummer" class="block mb-2 text-sm font-semibold text-gray-500">Telefoonnummer</label>
-            <input type="int" id="telnummer" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" [(ngModel)]="bedrijf.telefoonNr" required>
-
-            <label for="email" class="block mb-2 text-sm font-semibold text-gray-500">E-mail</label>
-            <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" [(ngModel)]="bedrijf.email" required>
-
+          <label for="bedrijfsnaam" class="font-semibold mt-5 block mb-2 text-sm text-gray-500">Bedrijfsnaam</label>
+          <input type="text" id="bedrijfsnaam" formControlName="naam" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          <div *ngIf="bedrijfForm.get('naam')?.invalid && (bedrijfForm.get('naam')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+            Bedrijfsnaam is verplicht.
           </div>
+          <label for="btwnummer" class="block mb-2 text-sm font-semibold text-gray-500">BTW-nummer</label>
+          <input type="text" id="btwnummer" formControlName="btwNummer" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          <div *ngIf="bedrijfForm.get('btwNummer')?.invalid && (bedrijfForm.get('btwNummer')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+            BTW-nummer is verplicht.
+          </div>
+          <div *ngIf="btwNummerInvalid" class="text-red-500 text-sm mt-1">
+            Ongeldig BTW-nummer.
+          </div>
+          <label for="adres" class="block mb-2 text-sm font-semibold text-gray-500">Adres</label>
+          <input type="text" id="adres" formControlName="adres" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          <div *ngIf="bedrijfForm?.get('adres')?.invalid && (bedrijfForm?.get('adres')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+            Adres is verplicht.
+          </div>
+
+          <label for="telnummer" class="block mb-2 text-sm font-semibold text-gray-500">Telefoonnummer</label>
+          <input type="tel" id="telnummer" formControlName="telefoonNr" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          <div *ngIf="bedrijfForm?.get('telefoonNr')?.invalid && (bedrijfForm?.get('telefoonNr')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+            Telefoonnummer is verplicht.
+          </div>
+
+          <label for="email" class="block mb-2 text-sm font-semibold text-gray-500">E-mail</label>
+          <input type="email" id="email" formControlName="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          <div *ngIf="bedrijfForm?.get('email')?.invalid && (bedrijfForm?.get('email')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+            E-mailadres is verplicht/ongeldig.
+          </div>
+          </div>
+    </form>
         </div>
         <div class="flex flex-col justify-center items-center h-full">
           <button type="submit" (click)="createBedrijf()" class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded mt-5">OK</button>
-
+          <button [routerLink]="'/'" class="bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">Terug</button> 
     </div>
   `,
   styles: [],
 })
 export class NieuwBedrijfComponent {  
-  bedrijf: any = {};
+  bedrijfForm: FormGroup;
+  btwNummerInvalid: boolean = false;
+  submitted: boolean = false;
 
-  constructor(private bedrijfService: BedrijfService, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private bedrijfService: BedrijfService, private router: Router, private http: HttpClient) {
+    this.bedrijfForm = this.formBuilder.group({
+      naam: ['', Validators.required],
+      btwNummer: ['', Validators.required],
+      adres: ['', Validators.required],
+      telefoonNr: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 
   createBedrijf() {
-    console.log(this.bedrijf);
-    this.bedrijf.status = 1;
-    this.bedrijfService.postBedrijf(this.bedrijf).subscribe({
-      next: () => {
-        this.router.navigate(['/']);
-      }
+    this.submitted = true;
+    if (this.bedrijfForm.invalid) {
+      console.error('Form invalid');
+      return;
+    }
+
+    const btwNummer = this.bedrijfForm.value.btwNummer;
+
+    //API om BTW nummer te valideren
+    this.http.get(`https://controleerbtwnummer.eu/api/validate/${btwNummer}.json`).subscribe({
+      next: (response: any) => {
+        if (response.valid) {
+          //als het BTW nummer geldig is, wordt het bedrijf aangemaakt
+          const bedrijf = this.bedrijfForm.value;
+          bedrijf.status = 1;
+
+          this.bedrijfService.postBedrijf(bedrijf).subscribe({
+            next: () => {
+              this.router.navigate(['/']);
+            },
+          });
+        } else {
+          this.btwNummerInvalid = true;
+          console.error('BTW number is invalid');
+        }
+      },
+      error: () => {
+        console.error('Something went wrong while validating the BTW number');
+      },
     });
   }
 }
--- Apps/AdminAngular/src/app/compontents/NieuwParkeerContract.component.ts
+++ Apps/AdminAngular/src/app/compontents/NieuwParkeerContract.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { BedrijfService } from '../services/bedrijf.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-nieuwparkeercontract',
@@ -11,37 +13,53 @@ import { Router } from '@angular/router';
         <select [(ngModel)]="selectedOption" (change)="onOptionSelect()" class="text-center mr-5 bedrijf-select ml-2 content-center bg-gray-50 border border-gray-300 rounded-lg focus:border-blue-500 block p-2.5 dark:bg-gray-400 hover:bg-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-600 dark:focus:border-gray-500" style="margin-left: 1.5rem;">
           <option *ngFor="let bedrijf of bedrijven" [value]="bedrijf.id">{{ bedrijf.naam }}</option>
         </select>
+        <form (ngSubmit)="maakParkingcontractAan()" [formGroup]="parkingContractForm" class="flex flex-col items-center">
+        <label for="parkeerplaatsen" class="font-semibold mt-5 block mb-2 text-sm text-black">Aantal parkeerplaatsen:</label>
+      <input formControlName="aantalPlaatsen" type="text" id="parkeerplaatsen" class="w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+        pattern="[0-9]*" inputmode="numeric">
+      <div *ngIf="parkingContractForm.get('aantalPlaatsen')?.invalid && (parkingContractForm.get('aantalPlaatsen')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+        Aantal parkeerplaatsen is verplicht.
+      </div>
 
-        <label for="parkeerplaatsen" class="font-semibold mt-5 block mb-2 text-sm text-black">Aantal Parkeerplaatsen:</label>
-            <input type="text" id="parkeerplaatsen" class="w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 " pattern="[0-9]*" inputmode="numeric" [(ngModel)]="aantalPlaatsen">
-            <label for="parkeerplaatsen" class="font-semibold mt-5 block mb-2 text-sm text-black">Locatie:</label>
-            <input type="text" id="locatie" class="w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 " pattern="[0-9]*" inputmode="numeric" [(ngModel)]="locatie">
+      <label for="parkeerplaatsen" class="font-semibold mt-5 block mb-2 text-sm text-black">Locatie:</label>
+      <input formControlName="locatie" type="text" id="locatie" class="w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 ">
+      <div *ngIf="parkingContractForm.get('locatie')?.invalid && (parkingContractForm.get('locatie')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+        Locatie is verplicht.
+      </div>
 
-            <label for="dates" class="font-semibold mt-5 block mb-2 text-sm text-black">Begin- en einddatum:</label>
-            <div class="flex items-center">
-              <div class="relative">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
-                  </svg>
-                </div>
-                <input name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
-                placeholder="Select start date" [(ngModel)]="startDatum">
-              </div>
-              <span class="mx-4 text-gray-500">tot</span>
-              <div class="relative">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                </div>
-                <input name="end" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
-                  placeholder="Select end date" [(ngModel)]="eindDatum">
-              </div>
-            </div>
-            <div class="flex flex-col justify-center items-center h-full">
-              <button (click)="maakParkingcontractAan()" type="input" class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">OK</button>
-              <button [routerLink]="'/'" class="bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">Terug</button> 
-            </div>
+      <label for="startDatum" class="font-semibold mt-5 block mb-2 text-sm text-black">Begin- en einddatum:</label>
+      <div class="flex items-center">
+        <div class="relative">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
+            </svg>
+          </div>
+          <input formControlName="startDatum" name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+            placeholder="Select start date">
+        </div>
+        <div *ngIf="parkingContractForm.get('startDatum')?.invalid && (parkingContractForm.get('startDatum')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+          Startdatum is verplicht.
+        </div>
+        <span class="mx-4 text-gray-500">tot</span>
+        <div class="relative">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
+            </svg>
+          </div>
+          <input formControlName="eindDatum" name="end" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+            placeholder="Select end date">
+        </div>
+        <div *ngIf="parkingContractForm.get('eindDatum')?.invalid && (parkingContractForm.get('eindDatum')?.touched || submitted)" class="text-red-500 text-sm mt-1">
+          Einddatum is verplicht.
+        </div>
+      </div>
+      <div class="flex flex-col justify-center items-center h-full">
+      <button type="input" class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">OK</button>
+        <button [routerLink]="'/'" class="bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">Terug</button>
+      </div>
+    </form>
   `,
   styles: [],
 })
@@ -56,8 +74,21 @@ export class NieuwParkeerContractComponent {
   eindDatum: Date | null = null;
   aantalPlaatsen: number = 0;
   locatie: string = '';
+  parkingContractForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor(private bedrijfService: BedrijfService, private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private bedrijfService: BedrijfService,
+    private router: Router
+  ) {
+    this.parkingContractForm = this.formBuilder.group({
+      aantalPlaatsen: ['', Validators.required],
+      locatie: ['', Validators.required],
+      startDatum: ['', Validators.required],
+      eindDatum: ['', Validators.required],
+
+    });
   }
 
   ngOnInit() {
@@ -94,19 +125,22 @@ export class NieuwParkeerContractComponent {
 
   
   maakParkingcontractAan() {
+    this.submitted = true;
+    if (this.parkingContractForm.invalid) {
+      console.error('Form invalid');
+      return;
+    }
   
     const parkingContract = {
-      id: 0,
-      bedrijfId: this.selectedOption,
-      aantalPlaatsen: this.aantalPlaatsen,
-      startDatum: this.startDatum,
-      eindDatum: this.eindDatum,
-      aantalBezettePlaatsen: 0,
+      ...this.parkingContractForm.value,
       status: 1,
-      locatie: this.locatie,
+      id: 0,
+      aantalBezettePlaatsen: 0,
+      bedrijfId: this.selectedOption,
     };
   
     console.log('Parking contract:', parkingContract);
+  
     this.bedrijfService.postParkingContract(parkingContract).subscribe({
       next: (response) => {
         console.log(response);
