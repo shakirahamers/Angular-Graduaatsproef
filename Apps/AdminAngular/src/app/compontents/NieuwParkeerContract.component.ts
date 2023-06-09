6d47375d44d5243e71df98a4ead44ca70729de59
kira <104847182+shakirahamers@users.noreply.github.com>
2023-06-09 11:20:21 +0200

    Parkeercontract

--- Apps/AdminAngular/src/app/compontents/NieuwParkeerContract.component.ts
+++ Apps/AdminAngular/src/app/compontents/NieuwParkeerContract.component.ts
@@ -25,17 +25,17 @@ import { Router } from '@angular/router';
                     <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
                   </svg>
                 </div>
-                <input name="start" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
-                  bsDatepicker placeholder="Select start date" [(ngModel)]="startDatum">
+                <input name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                placeholder="Select start date" [(ngModel)]="startDatum">
               </div>
-              <span class="mx-4 text-gray-500">to</span>
+              <span class="mx-4 text-gray-500">tot</span>
               <div class="relative">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                   <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </div>
-                <input name="end" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
-                  bsDatepicker placeholder="Select end date" [(ngModel)]="eindDatum">
+                <input name="end" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                  placeholder="Select end date" [(ngModel)]="eindDatum">
               </div>
             </div>
             <div class="flex flex-col justify-center items-center h-full">
@@ -92,25 +92,30 @@ export class NieuwParkeerContractComponent {
     });
   }
 
+  
   maakParkingcontractAan() {
+  
     const parkingContract = {
       id: 0,
       bedrijfId: this.selectedOption,
       aantalPlaatsen: this.aantalPlaatsen,
-      startDate: this.startDatum,
-      endDate: this.eindDatum,
+      startDatum: this.startDatum,
+      eindDatum: this.eindDatum,
       aantalBezettePlaatsen: 0,
       status: 1,
       locatie: this.locatie,
     };
-
+  
+    console.log('Parking contract:', parkingContract);
     this.bedrijfService.postParkingContract(parkingContract).subscribe({
       next: (response) => {
         console.log(response);
+        this.router.navigate(['/alleparkingcontracten']);
       },
       error: (error) => {
         console.error('Error creating parking contract:', error);
       },
     });
   }
+  
 }
